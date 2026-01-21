ad5e04adb3c64cd47d2b2fdb461b845ee0a5ef8e
zahrahabib21 <helaizahra112@gmail.com>
2026-01-22 00:59:10 +0430

    implement profile page component and update app  and add profile page UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import avatarimage from './assets/avater.png'
+import profilepage from './components/profilepage'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const user = {
+    name: "Zahra Hablb",
+    username: "@helaizahra",
+    avatarimage: avatarimage,
+    xp:90,
+    lastActive: "Dec 28 2025",
+    streakDays: 2,
+    longesStreak: 2,
+    recentBadgesText: "vo badges earned yet.",
+
+    personalinfo: {
+     Email: 'helaizahra21@gmail.com', 
+     Gender: 'female',
+     Country: 'Afghanistan',
+     "Province or State": 'Kabul',
+     "Working Expreiance": "1",
+     "Date of Birth": "6/6/2002",
+     "Online Protfolio Link": "N/A",
+     "Likedin Link": "N/A",
+     Languages: "Farsi,Pashto,English,Urdo",
+     Bio: "Computer Science Student|Future Web Developer|"
+
+
+
+
+    },
+
+    accountDetails:{
+      Role: "Student",
+      Status:"Active",
+      Verification: "Verified"
+
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='page'>
+     <profilepage user={user}></profilepage>
+    </div>
   )
+
 }
 
 export default App
